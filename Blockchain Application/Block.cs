f8797ec60644c6726a28a14288eb56830a8661d1
RuslanEyvazli <rus.eyvazli@gmail.com>
2023-07-09 22:36:19 +0400

    Add project files.

---
+++ Blockchain Application/Block.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Blockchain_Application
+{
+    public class Block
+    {
+        public int Index { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string PreviousHash { get; set; }
+        public string Hash { get; set; }
+        public string Data { get; set; }
+
+        public Block(DateTime timeStamp, string previousHash, string data)
+        {
+            this.Index = 0;
+            this.TimeStamp = timeStamp;
+            this.PreviousHash = previousHash;
+            this.Data = data;
+            this.Hash = CalculateHash(); 
+        }
+        public string CalculateHash()
+        {
+            SHA256 sha256 = SHA256.Create();
+            byte[] inBytes = Encoding.ASCII.GetBytes($"{TimeStamp}-{PreviousHash ?? ""}-{Data}");
+            byte[] outBytes = sha256.ComputeHash( inBytes );
+            return Convert.ToBase64String(outBytes);
+        }
+    }
+}
---
+++ Blockchain Application/Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+
+namespace Blockchain_Application
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Adding new block to our blockchain
+            Blockchain blockchain = new Blockchain();
+            blockchain.AddBlock(new Block(DateTime.Now, null, "{ sender: Ruslan, receive: Eyvaz, amount: 2200 }"));
+            blockchain.AddBlock(new Block(DateTime.Now, null, "{ sender: Micheal, receive: Phil, amount: 1200 }"));
+            blockchain.AddBlock(new Block(DateTime.Now, null, "{ sender: Jay, receive: Kelman, amount: 200 }"));
+
+
+            Console.WriteLine(JsonConvert.SerializeObject(blockchain,Formatting.Indented));
+            // Try to check validation state and result is true.
+            Console.Write(blockchain.IsValid()+ "\n");
+
+            // Try to chech validation state after changed only one block in chain.Result is False.  
+            blockchain.Chain[1].Data = "{ sender: Micheal, receive: Phill, amount: 1200 }";
+            Console.Write(blockchain.IsValid() + "\n");
+            Console.WriteLine(JsonConvert.SerializeObject(blockchain, Formatting.Indented));
+
+            // Try to update all block in blockchain.( 51% Attack )
+            blockchain.Chain[1].Hash = blockchain.Chain[1].CalculateHash();
+            blockchain.Chain[2].PreviousHash = blockchain.Chain[1].Hash;
+            blockchain.Chain[2].Hash = blockchain.Chain[2].CalculateHash();
+            blockchain.Chain[3].PreviousHash = blockchain.Chain[2].Hash;
+            blockchain.Chain[3].Hash = blockchain.Chain[3].CalculateHash();
+            
+            // Try to chech validation state after changed only one block in chain.Result is True (Bumppp).  
+            Console.WriteLine(JsonConvert.SerializeObject(blockchain, Formatting.Indented));
+            Console.Write(blockchain.IsValid());
+            
+            Console.ReadKey();  
+        }
+    }
+}
